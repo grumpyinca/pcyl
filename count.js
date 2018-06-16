e4297a234c729013a033aa75d178eb4002d82235
Grumpy In CA <WinTenTester@outlook.com>
2018-06-16 14:07:10 -0700

    12 added count.js; modified start.js; debug output remains for now.

---
+++ count.js
@@ -0,0 +1,47 @@
+/**
+ * Set nfixed, nstf and nfdcl by direct count
+ * Insures that these values are consistent, eliminates need for bookkeeping.
+ */
+
+function count() {
+    
+    NFDCL  = 0;
+    NFIXED = 0;
+    for ( var property in design_parameters) {
+        if (design_parameters.hasOwnProperty(property)) {
+            var dp = design_parameters[property];
+            if (dp.lmin == FIXEDSTAT) {
+                NFIXED++;
+            }
+            if (dp.lmin < FREESTAT){
+                NFDCL++;
+            }
+            if (dp.lmax < FREESTAT){
+                NFDCL++;
+            }
+        }
+    }
+
+    NSTF = 0;
+    for ( var property in state_variables) {
+        if (state_variables.hasOwnProperty(property)) {
+            var sv = state_variables[property];
+            if (sv.lmin == FIXEDSTAT) {
+                NSTF++;
+            }
+            if (sv.lmin < FREESTAT){
+                NFDCL++;
+            }
+            if (sv.lmax < FREESTAT){
+                NFDCL++;
+            }
+        }
+    }
+
+// console.log('design parameters = ', design_parameters);
+// console.log('state variables = ', state_variables);
+// console.log('FREESTAT, SETSTAT, FIXEDSTAT: ', FREESTAT, SETSTAT, FIXEDSTAT);
+console.log('count: NFIXED, NSTF, NFDCL = ', NFIXED, NSTF, NFDCL);
+}
+
+module.exports = count;
--- start.js
+++ start.js
@@ -2,12 +2,20 @@
  * START command - initializes primary system variables; reads startup file
  */
 
-//const eqnset1 = require('./eqnset1');
+//const despak = require('./despak');
+const count = require('./count');
 
 function start(split_line) {
 
 console.log("START:");
 console.log("  The START command is not yet implemented.")
+
+// design_parameters.radius.lmin = 2;
+// state_variables.force.lmin = 2;
+// state_variables.stress.lmin = -1;
+
+count();
+console.log('start: NFIXED, NSTF, NFDCL = ', NFIXED, NSTF, NFDCL);
 
 }
 
