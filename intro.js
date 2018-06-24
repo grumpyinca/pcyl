b1c61d8513c01f6ab41ab7dcceefc6ade741cc0e
Brian Watt <bwatt@1fifoto.com>
2018-06-24 08:42:51 -0500

    41 Make PCyl intro match DOS

--- intro.js
+++ intro.js
@@ -38,9 +38,26 @@ global.NFDCL = 0;
 
 //intro: procedure(sysprompt,version);
 function intro() {
-    console.log('**********************************************');
-    console.log(`***  Welcome to ${design_name} Version ${version}`);
-    console.log('**********************************************');
+    console.log();
+    console.log('                             P C y l');
+    console.log();
+    console.log('         A DECISION SUPPORT SYSTEM FOR PISTON/CYLINDER DESIGN');
+    console.log();
+    console.log('                      A product of Michael Milley');
+    console.log('               125 Jardin Dr.   Los Altos,  Ca.    94022');
+    console.log();
+    console.log('         Copyright 1990  Michael Milley    All rights reserved.');
+    console.log('                       License agreement required.');
+    console.log();
+    console.log(`                       VERSION ${version}`);
+    console.log('                   ');
+    console.log();
+    console.log('     Please review the  RESTRICTIONS  section of the documentation.');
+    console.log('     Validity, accuracy or applicability of results of this program are');
+    console.log('     NOT guaranteed.  The user must verify all results by independent');
+    console.log('     means and assume all responsibilities for their use and consequences.');
+    console.log('     No warranty is provided.  No liability is assumed.');
+    console.log();
     //
     // declare  sysprompt character(16) varying,
     //        version   character( 4) varying,
