d365651527701a89aad32356f5fd0d24f4d384fd
Brian Watt <bwatt@1fifoto.com>
2018-08-17 12:31:49 -0500

    111 Add braces to smalest and bigest if statements in trade.js

--- trade.js
+++ trade.js
@@ -233,33 +233,41 @@ function trade(split_line) {
                     j = vflag[i];
                     if (j < design.design_parameters.length) {
                         var dp = design.design_parameters[j];
-                        if (ldir[i] < 0)
-                            if (temp2 > SMALLNUM)
+                        if (ldir[i] < 0) {
+                            if (temp2 > SMALLNUM) {
                                 temp = dp.vmin / temp2;
-                            else
+                            } else {
                                 temp = dp.vmin;
-                        else 
-                            if (temp2 > SMALLNUM)
+                            }
+                        } else {
+                            if (temp2 > SMALLNUM) {
                                 temp = dp.vmax / temp2;
-                            else
+                            } else {
                                 temp = dp.vmax;
+                            }
+                        }
                     } else {
                         var sv = design.state_variables[j - design.design_parameters.length];
-                        if (ldir[i] < 0)
-                            if (temp2 > SMALLNUM)
+                        if (ldir[i] < 0) {
+                            if (temp2 > SMALLNUM) {
                                 temp = sv.vmin / temp2;
-                            else
+                            } else {
                                 temp = sv.vmin;
-                        else 
-                            if (temp2 > SMALLNUM)
+                            }
+                        } else {
+                            if (temp2 > SMALLNUM) {
                                 temp = sv.vmax / temp2;
-                            else
+                            } else {
                                temp = sv.vmax;
+                            }
+                        }
                     }
-                    if (temp > SMALLNUM && temp < smalest)
+                    if (temp > SMALLNUM && temp < smalest) {
                         smalest = temp;
-                    if (temp > bigest)
+                    }
+                    if (temp > bigest) {
                         bigest = temp;
+                    }
                 }
                 j = vflag[itemp];
                 if (j < design.design_parameters.length) {
